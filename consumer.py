1826a12389bbde1605a84d65ac878bb6dcbf1928
Lindsey Sullivan <sullivan.lindseynicole@gmail.com>
2023-10-02 20:00:30 -0500

    Update for Module 07 - 10/2/2023

---
+++ consumer.py
@@ -0,0 +1,221 @@
+"""
+
+"""
+
+# import libraries
+import csv
+import pika
+import os
+import sys
+import threading
+import smtplib # Import smtplib to send an email with the output CSV files
+import glob  # Import the glob module to easily list all CSV files in the output directory
+import signal # Import signal to have worker to stop their execution and then send the email
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from dotenv import load_dotenv
+
+from util_logger import setup_logger
+logger, logname = setup_logger(__file__)
+
+# Load .env file
+load_dotenv()
+
+# Retrieve environment variables
+FROM_EMAIL = os.getenv('EMAIL')
+TO_EMAIL = os.getenv('TOEMAIL')
+PASSWORD = os.getenv('PASSWORD')
+
+# Check if email and password are set
+if not FROM_EMAIL or not PASSWORD:
+    print("Error: EMAIL and PASSWORD must be set in the .env file.")
+    sys.exit(1)
+
+# -------------------------------------
+# Define Constants
+# -------------------------------------
+
+# define email and SMTP details
+
+SMTP_SERVER = "smtp.gmail.com"
+PORT = 587
+
+# define constants
+HOST = "localhost"
+INTERMEDIATE_FILE_PATH = "/Users/lindseysullivan/Documents/School/Streaming-Data/Modules/streaming-07-life-expectancy/intermediate_file.csv"
+
+# define criteria
+LIFE_EXPECTANCY_CRITERIA = 72.72
+GDP_PER_CAPITA_CRITERIA = 10881
+
+# Creating a directory to store output files
+os.makedirs("output", exist_ok=True)
+
+# Flag to signal all threads to stop
+shutdown_flag = False
+
+def signal_handler(signal, frame):
+    global shutdown_flag
+    print("\nCTRL+C detected. Preparing to shut down.")
+    shutdown_flag = True
+
+# Register signal handler to detect CTRL+C
+signal.signal(signal.SIGINT, signal_handler)
+
+def send_email(subject, body, attachment_path, to_email, from_email, smtp_server, port, login, password, queue_name):
+    """
+    
+    """
+    full_subject = f"{subject} - {queue_name}"
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = full_subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    if os.path.isfile(attachment_path):
+        with open(attachment_path, "rb") as attachment:
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(attachment.read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', f"attachment; filename= {os.path.basename(attachment_path)}")
+            msg.attach(part)
+
+    try:
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls()
+            server.login(login, password)
+            text = msg.as_string()
+            server.sendmail(from_email, to_email, text)
+            logger.info(f"Email sent to {to_email}")
+    except Exception as e:
+        logger.error(f"Failed to send email: {e}")
+
+
+def callback(ch, method, properties, body):
+    """
+    
+    """
+    try:
+        message = body.decode()
+        row = message.split(',')
+
+        country = row[0].strip()
+        region = row[1].strip()
+        year = row[2].strip()
+        life_expectancy = float(row[3].strip())  
+        gdp_per_capita = float(row[4].strip())   
+
+        if life_expectancy > LIFE_EXPECTANCY_CRITERIA and gdp_per_capita > GDP_PER_CAPITA_CRITERIA:
+            filename = f"output/{region}.csv"
+
+            mode = 'a' if os.path.exists(filename) else 'w'
+            with open(filename, mode, newline='', encoding="utf-8") as file:
+                writer = csv.writer(file)
+                
+                if mode == 'w':
+                    writer.writerow(["Country", "Region", "Year", "GDP_per_capita", "Life_expectancy"])
+
+                writer.writerow([country, region, year, gdp_per_capita, life_expectancy])
+                logger.info(f"Data written for {country} in {region}")
+    
+    except IndexError:
+        logger.error(f"Index error with row: {row}. Ensure the expected CSV structure is correct.")
+
+    except ValueError as e:
+        logger.error(f"Value error: {e}. Ensure the data types and values in the row are correct: {row}")
+
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {e} with row {row}")
+
+
+def worker(host, queue_name):
+    """
+    """
+    global shutdown_flag
+
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host))
+        channel = connection.channel()
+    except Exception as e:
+        print("\nERROR: Connection to RabbitMQ server failed.")
+        print(f"Verify the server is running on host={host}.")
+        print(f"The error says: {e}")
+        sys.exit(1)
+
+    try:
+        channel.queue_declare(queue=queue_name, durable=True)
+
+        channel.basic_consume(
+            queue=queue_name,
+            on_message_callback=callback,
+            auto_ack=True
+        )
+
+        print(f"\nWaiting for messages in {queue_name}. To exit press CTRL+C")
+
+        while not shutdown_flag:
+            channel.connection.process_data_events(time_limit=1)
+            
+    except KeyboardInterrupt:
+        shutdown_flag = True
+        print("\nUser interrupted continuous listening process.")
+
+    except Exception as e:
+        print(f"ERROR: {e}")
+
+    finally:
+        print("\nClosing connection. Goodbye.\n")
+        connection.close()
+
+        
+
+   
+    
+# Standard Python idiom to indicate main program entry point
+# This allows us to import this module and use its functions
+# without executing the code below.
+# If this is the program being run, then execute the code below
+
+if __name__ == "__main__":
+    
+    # call the main function with the information needed
+    regions = set()
+
+    if os.path.exists(INTERMEDIATE_FILE_PATH):
+        with open(INTERMEDIATE_FILE_PATH, mode='r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip header
+            regions = {row[1] for row in reader}
+
+        threads = []
+        for region in regions:
+            queue_name = f"queue_{region.replace(' ', '_')}"
+            thread = threading.Thread(target=worker, args=(HOST, queue_name))
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+        if shutdown_flag:
+            try:
+                files = glob.glob("output/*.csv")
+
+                if files:
+                    subject = "Life Expectancy/GDP by Countries - Greater than 2020 Average - Data Export"
+                    body = "The consumer has been interrupted. Attached are the CSV files with the data gathered so far."
+
+                    for file in files:
+                        send_email(subject, body, file, TO_EMAIL, FROM_EMAIL, SMTP_SERVER, PORT, FROM_EMAIL, PASSWORD, queue_name)
+                        print(f"\nEmail sent with attachment: {file}")
+                else:
+                    print("No CSV files found in the output directory to send.")
+            except Exception as e:
+                print(f"Error sending email: {e}")
+    else:
+        logger.error(f"The file {INTERMEDIATE_FILE_PATH} does not exist or is not accessible.")
+    
---
+++ producer.py
@@ -0,0 +1,115 @@
+"""
+
+"""
+
+# import libraries
+import pika
+import sys
+import webbrowser
+import csv
+import os
+import time
+
+from util_logger import setup_logger
+logger, logname = setup_logger(__file__)
+
+
+# declare constants
+FILE_PATH = "/Users/lindseysullivan/Documents/School/Streaming-Data/Modules/streaming-07-life-expectancy"
+INPUT_CSV_FILE = "Life-Expectancy-Data-Updated.csv"
+HOST = "localhost"
+INTERMEDIATE_CSV_FILE = "intermediate_file.csv"
+
+# setup path to directory for CSV file
+os.chdir(FILE_PATH)
+
+# ----------------------------------------------
+# Define Program Functions
+# ----------------------------------------------
+def offer_rabbitmq_admin_site(show_offer):
+    """
+    Offer to open the RabbitMQ Admin website
+    """
+    if show_offer == True:
+         ans = input("Would you like to monitor RabbitMQ queues? y or n ")
+         print()
+         if ans.lower() == "y":
+            webbrowser.open_new("http://localhost:15672/#/queues")
+            print()
+
+def send_to_queue(host, queue_name, message):
+    """
+    
+    """
+    conn = pika.BlockingConnection(pika.ConnectionParameters(host))
+    channel = conn.channel()
+    channel.queue_declare(queue=queue_name, durable=True)
+    channel.basic_publish(exchange='',
+                          routing_key=queue_name,
+                          body=message,
+                          properties=pika.BasicProperties(
+                              delivery_mode=2,  # make message persistent
+                          ))
+    conn.close()
+
+
+def stream_csv_messages(input_file_name: str, intermediate_file_name: str, host: str):
+    """
+    Read input CSV file, save to an intermediate file, and send each row as a message to a queue based on the region.
+
+    Parameters:
+        input_file_name (str): The name of the original CSV file
+        intermediate_file_name (str): The name of the intermediate CSV file
+        host (str): host name or IP address of the RabbitMQ server
+    """
+    try:
+        with open(input_file_name, "r", encoding="utf-8") as infile, \
+             open(intermediate_file_name, "w", newline='', encoding="utf-8") as outfile:
+
+            reader = csv.reader(infile, delimiter=",")
+            writer = csv.writer(outfile)
+
+            headers = next(reader, None)  # skip header row
+            # write specific header row columns to intermediate file
+            writer.writerow(["Country", "Region", "Year", "Life Expectancy", "GDP Per Capita"])  
+
+            for row in reader:
+                # write specific data row columns to intermediate file
+                country = row[headers.index('Country')]
+                region = row[headers.index('Region')]
+                year = row[headers.index('Year')]
+                life_expectancy = row[headers.index('Life_expectancy')]
+                gdp_per_capita = row[headers.index('GDP_per_capita')]
+
+                writer.writerow([country, region, year, life_expectancy, gdp_per_capita])
+
+                queue_name = f"queue_{region.replace(' ', '_')}"
+                message = ",".join([country, region, year, life_expectancy, gdp_per_capita])
+                send_to_queue(host, queue_name, message)
+                logger.info(f"Sent message for {country} to {queue_name}")
+
+                time.sleep(1)
+
+            logger.info(f"Data written to {intermediate_file_name}")
+
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+            
+          
+
+    except pika.exceptions.AMQPConnectionError as e:
+        print(f"Error: Connection to RabbitMQ server failed: {e}")
+        sys.exit(1)
+    
+
+# Standard Python idiom to indicate main program entry point
+# This allows us to import this module and use its functions
+# without executing the code below.
+# If this is the program being run, then execute the code below
+if __name__ == "__main__":  
+    # ask the user if they'd like to open the RabbitMQ Admin site
+    # true shows the offer/false turns off the offer for the user
+    offer_rabbitmq_admin_site(show_offer=True)
+
+    # Stream messages from the CSV file and send them to RabbitMQ
+    stream_csv_messages(INPUT_CSV_FILE, INTERMEDIATE_CSV_FILE, HOST)
